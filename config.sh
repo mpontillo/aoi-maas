4da445234e4a446e89bb0505233ba5af93866adf
Mike Pontillo <pontillo@gmail.com>
2016-09-22 13:58:04 -0700

    Remove redundant '...' characters.

--- config.sh
+++ config.sh
@@ -7,12 +7,16 @@ API_KEY="$(aoi config get maas_api_key '')"
 
 PROFILE="$(aoi config get maas_profile admin)"
 
+MAAS_SERVER_CPUS="$(aoi config get maas_server_cpus 4)"
+MAAS_SERVER_RAM="$(aoi config get maas_server_ram 4096)"
+
 TESTNET_CIDR="172.16.99.0/24"
 TESTNET_IP=172.16.99.2
 TESTNET_GW=172.16.99.1
 TESTNET_DYN_START=172.16.99.150
 TESTNET_DYN_END=172.16.99.199
 TESTNET_NETMASK=255.255.255.0
+
 
 BOOT_SOURCE_URL="$(aoi config get maas_images_url '')"
 UBUNTU_ARCHIVE_HOST="$(aoi config get ubuntu_archive_host '')"
