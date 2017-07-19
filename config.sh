a5dd307dbd97447a05b28273add524047ee33060
Mike Pontillo <pontillo@gmail.com>
2017-07-19 17:38:34 +0200

    Stability and multi-instance fixes.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 IMAGES_DIR="$(aoi config get images_dir ~/.cloud-images)"
-INSTANCE="$(aoi config get maas_instance maas)"
+export INSTANCE="$(aoi config get maas_instance maas)"
 DISTRO="$(aoi config get maas_distro xenial)"
 MAAS_USER="$(aoi config get maas_user admin)"
 MAAS_PASSWORD="$(aoi config get maas_password test)"
@@ -22,6 +22,8 @@ TESTNET_NETMASK=255.255.255.0
 BOOT_SOURCE_URL="$(aoi config get maas_images_url '')"
 UBUNTU_ARCHIVE_HOST="$(aoi config get ubuntu_archive_host '')"
 PPA="$(aoi config get maas_ppa ppa:maas-maintainers/experimental3)"
+MAAS_REPO="$(aoi config get maas_repo '')"
+MAAS_REPO_KEY="$(aoi config get maas_repo_key '')"
 
 LIBVIRT_USER="$(aoi config get libvirt_remote_user libvirt-qemu)"
 LIBVIRT_PASSWORD="$(aoi config get libvirt_remote_password qwe123)"
